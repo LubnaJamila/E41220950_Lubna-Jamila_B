28ab81c7e9de061eef61ef225a389f0ca32d701c
LubnaJamila <124659215+LubnaJamila@users.noreply.github.com>
2023-02-10 21:12:16 +0700

    Create Testing.java

---
+++ minggu 1/Testing.java
@@ -0,0 +1,10 @@
+package testing;
+public class Testing {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 2;
+        int b = 3;
+        System.out.println(a*b);
+    }
+    
+}
